c3b91944fe2b1de838c6c36eb798ab8ed55e1d6b
Piotr Grabowski <piotr.grabowski@scylladb.com>
2020-12-08 17:59:26 +0100

    Initial work on introducing types with translation
    Some types returned by Java Driver, such as UDTs return non-native
    Java types. Therefore they need "translating" to our common 
    representation. This translation is handled by added 
    translateDriverObject method.
    
    Added Duration and CqlDate classes that represent DURATION and DATE
    CQL types.
    
    Added getUDT(), getTimestamp(), getTime(), getDuration(), getDate()
    methods to RawChange.

--- scylla-cdc-base/src/main/java/com/scylladb/cdc/cql/driver3/Driver3RawChange.java
+++ scylla-cdc-base/src/main/java/com/scylladb/cdc/cql/driver3/Driver3RawChange.java
@@ -2,19 +2,23 @@ package com.scylladb.cdc.cql.driver3;
 
 import static com.datastax.driver.core.Metadata.quoteIfNecessary;
 
-import com.datastax.driver.core.CodecRegistry;
+import com.datastax.driver.core.Duration;
+import com.datastax.driver.core.LocalDate;
 import com.datastax.driver.core.Row;
+import com.datastax.driver.core.UDTValue;
+import com.datastax.driver.core.UserType;
 import com.google.common.base.Preconditions;
-import com.google.common.reflect.TypeToken;
 import com.scylladb.cdc.model.StreamId;
 import com.scylladb.cdc.model.worker.RawChange;
 import com.scylladb.cdc.model.worker.ChangeId;
 import com.scylladb.cdc.model.worker.ChangeSchema;
 
 import java.nio.ByteBuffer;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 public final class Driver3RawChange implements RawChange {
     private final Row row;
@@ -41,8 +45,65 @@ public final class Driver3RawChange implements RawChange {
         if (row.isNull(columnName)) {
             return null;
         } else {
-            return row.getObject(columnName);
+            return translateDriverObject(row.getObject(columnName));
         }
+    }
+
+    private Object translateDriverObject(Object driverObject) {
+        // Some types returned by getObject() are
+        // some classes of Java Driver. We should
+        // translate them into a non-Java-Driver-specific
+        // types. (for example UDTValue)
+
+        if (driverObject == null) {
+            return null;
+        }
+
+        // Even though List, Map, Set are
+        // a standard Java library types,
+        // they can contain values that
+        // are Java-Driver-specific.
+        //
+        // For example: map<int, some_udt>
+        //
+        // Recursively call translateObject on their elements.
+        if (driverObject instanceof List) {
+            List<Object> driverList = (List<Object>)driverObject;
+            return driverList.stream().map(this::translateDriverObject).collect(Collectors.toList());
+        } else if (driverObject instanceof Set) {
+            Set<Object> driverSet = (Set<Object>)driverObject;
+            return driverSet.stream().map(this::translateDriverObject).collect(Collectors.toSet());
+        } else if (driverObject instanceof Map) {
+            Map<Object, Object> driverMap = (Map<Object, Object>)driverObject;
+            // Deliberately using LinkedHashMap to preserve the same order as driverObject.
+            Map<Object, Object> translatedMap = new LinkedHashMap<>();
+            for (Map.Entry<Object, Object> entry : driverMap.entrySet()) {
+                translatedMap.put(translateDriverObject(entry.getKey()), translateDriverObject(entry.getValue()));
+            }
+            return translatedMap;
+        }
+
+        // Java-Driver-specific types
+        if (driverObject instanceof UDTValue) {
+            UDTValue driverUDT = (UDTValue) driverObject;
+            UserType driverUserType = driverUDT.getType(); // Could instead use the ChangeSchema information
+            Map<String, Object> translatedMap = new LinkedHashMap<>();
+            for (UserType.Field field : driverUserType) {
+                Object fieldValue = translateDriverObject(driverUDT.getObject(field.getName()));
+                translatedMap.put(field.getName(), fieldValue);
+            }
+            return translatedMap;
+        } else if (driverObject instanceof com.datastax.driver.core.Duration) {
+            com.datastax.driver.core.Duration driverDuration = (com.datastax.driver.core.Duration) driverObject;
+            return new Duration(driverDuration.getMonths(), driverDuration.getDays(), driverDuration.getNanoseconds());
+        } else if (driverObject instanceof LocalDate) {
+            LocalDate driverDate = (LocalDate) driverObject;
+            return new CqlDate(driverDate.getYear(), driverDate.getMonth(), driverDate.getMonth());
+        }
+
+        // No translation needed, the object is
+        // already of a good type.
+        return driverObject;
     }
 
     @Override
--- scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/RawChange.java
+++ scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/RawChange.java
@@ -10,6 +10,7 @@ import java.nio.ByteBuffer;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -43,6 +44,108 @@ public interface RawChange {
         public static OperationType parse(byte value) {
             // TODO - validation
             return enumValues[value];
+        }
+    }
+
+    /*
+     * Represents CQL type DURATION
+     */
+    class Duration {
+        // months, days and nanoseconds
+        private final long months;
+        private final long days;
+        private final long nanoseconds;
+
+        public Duration(long months, long days, long nanoseconds) {
+            this.months = months;
+            this.days = days;
+            this.nanoseconds = nanoseconds;
+        }
+
+        public long getMonths() {
+            return months;
+        }
+
+        public long getDays() {
+            return days;
+        }
+
+        public long getNanoseconds() {
+            return nanoseconds;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Duration duration = (Duration) o;
+            return months == duration.months &&
+                    days == duration.days &&
+                    nanoseconds == duration.nanoseconds;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(months, days, nanoseconds);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Duration(%d, %d, %d)", months, days, nanoseconds);
+        }
+    }
+
+    /*
+     * Represents CQL type DATE
+     */
+    class CqlDate {
+        private final int year;
+        private final int month;
+        private final int day;
+
+        public CqlDate(int year, int month, int day) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+
+        public int getDay() {
+            return day;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CqlDate cqlDate = (CqlDate) o;
+            return year == cqlDate.year &&
+                    month == cqlDate.month &&
+                    day == cqlDate.day;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(year, month, day);
+        }
+
+        private String pad(String s) {
+            if (s.length() < 2) {
+                return "0" + s;
+            }
+            return s;
+        }
+
+        @Override
+        public String toString() {
+            return year + "-" + pad(Integer.toString(month)) + "-" + pad(Integer.toString(day));
         }
     }
 
@@ -131,6 +234,26 @@ public interface RawChange {
 
     default List getList(String columnName) {
         return (List) getAsObject(columnName);
+    }
+
+    default Map<String, Object> getUDT(String columnName) {
+        return (Map<String, Object>) getAsObject(columnName);
+    }
+
+    default Date getTimestamp(String columnName) {
+        return (Date) getAsObject(columnName);
+    }
+
+    default Long getTime(String columnName) {
+        return (Long) getAsObject(columnName);
+    }
+
+    default Duration getDuration(String columnName) {
+        return (Duration) getAsObject(columnName);
+    }
+
+    default CqlDate getDate(String columnName) {
+        return (CqlDate) getAsObject(columnName);
     }
 
     /*
