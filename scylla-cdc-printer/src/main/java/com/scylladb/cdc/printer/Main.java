79e98e7c0d962d508c0a8ccf424418fdd5b8300a
Piotr Grabowski <piotr.grabowski@scylladb.com>
2020-12-17 14:55:12 +0100

    Rename Printer to Main
    Rename Printer to Main to be more consistent between Printer and
    Replicator.

--- scylla-cdc-printer/src/main/java/com/scylladb/cdc/printer/Main.java
+++ scylla-cdc-printer/src/main/java/com/scylladb/cdc/printer/Main.java
@@ -31,7 +31,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Printer {
+public class Main {
     public static void main(String[] args) {
         // Turn off logging for the standard output
         // to be more clear.
