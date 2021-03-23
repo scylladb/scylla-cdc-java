1dce801c688bb9d8464fddba8f49f2daa4e1b68e
Calle Wilund <calle@cwitec.com>
2021-03-23 10:24:10 +0100

    Add cell accessors to get "meta" cell for deletion
    Thus, given a cell representing a collection column, you can ask
    directly for deleted elements of this column. Building towards being
    able to clearly separate "source" vs. "cdc meta" columns/cells.

--- scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/cql/Cell.java
+++ scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/cql/Cell.java
@@ -1,7 +1,15 @@
 package com.scylladb.cdc.model.worker.cql;
 
+import java.util.Set;
+
 import com.scylladb.cdc.model.worker.ChangeSchema;
 
 public interface Cell extends Field {
     ChangeSchema.ColumnDefinition getColumnDefinition();
+
+    Set<Field> getDeletedElements();
+
+    boolean hasDeletedElements();
+
+    boolean isDeleted();
 }
