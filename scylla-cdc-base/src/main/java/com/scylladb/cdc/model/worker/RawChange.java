4ac73a218aba5e88e2bd26b0041cb74efcaa1661
Calle Wilund <calle@cwitec.com>
2021-03-23 10:25:38 +0100

    Add "raw bytes" accessor to cells/change
    To allow reading unmarshalled cql data raw

--- scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/RawChange.java
+++ scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/RawChange.java
@@ -1,5 +1,7 @@
 package com.scylladb.cdc.model.worker;
 
+import java.nio.ByteBuffer;
+
 import com.scylladb.cdc.model.worker.cql.Cell;
 
 /*
@@ -56,16 +58,22 @@ public interface RawChange {
     Object getAsObject(ChangeSchema.ColumnDefinition c);
 
     default Cell getCell(String columnName) {
-        return getCell(getSchema().getColumnDefinition(columnName));        
+        return getCell(getSchema().getColumnDefinition(columnName));
     }
 
     Cell getCell(ChangeSchema.ColumnDefinition c);
 
     default boolean isNull(String columnName) {
-        return isNull(getSchema().getColumnDefinition(columnName));                
+        return isNull(getSchema().getColumnDefinition(columnName));
     }
 
     boolean isNull(ChangeSchema.ColumnDefinition c);
+
+    default ByteBuffer getUnsafeBytes(String columnName) {
+        return getUnsafeBytes(getSchema().getColumnDefinition(columnName));
+    }
+
+    ByteBuffer getUnsafeBytes(ChangeSchema.ColumnDefinition c);
 
     default boolean getIsDeleted(String columnName) {
         String deletedColumnName = "cdc$deleted_" + columnName;
