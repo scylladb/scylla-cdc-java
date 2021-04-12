f8d2c5e216f13970f3b72d7f9e7e9fc70ea8e3e2
Calle Wilund <calle@cwitec.com>
2021-04-12 13:55:42 +0200

    Add sequence wrapper for N changes and lister/adaptors for it
    Allows user code to subscribe to only full CDC batch sequences. Also
    ensures we only update task state in user code on completed batch
    sequqence.
    The idea being that should we fail and crash  while still building a
    sequence to report, we should not update any state that determines where
    to restart until we've reached sequence end. Once we add some proper
    state management, individual stream positions can be quantized on
    sequence boundary.
    
    This can then be adapted on a higher level, to break the sequence down
    into individual delete/insert/update(s) in case of actual batching. For
    "normal" CQL a batch sequence already matches a single operation.

--- scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/Consumer.java
+++ scylla-cdc-base/src/main/java/com/scylladb/cdc/model/worker/Consumer.java
@@ -1,8 +1,12 @@
 package com.scylladb.cdc.model.worker;
 
+import static java.util.Collections.singletonList;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 /**
@@ -36,6 +40,31 @@ public class Consumer {
         });
     }
 
+    public Consumer(BatchSequenceConsumer batchSequenceConsumer) {
+        this(new ConsumerDispatch() {
+            private final ConcurrentHashMap<ChangeId, List<RawChange>> buffer = new ConcurrentHashMap<>();
+
+            @Override
+            public CompletableFuture<TaskState> consume(Task task, RawChange change) {
+                if (change.isEndOfBatch()) {
+                    List<RawChange> changes = buffer.remove(change.getId());
+                    if (changes == null) {
+                        changes = singletonList(change);
+                    } else {
+                        changes.add(change);
+                    }
+                    batchSequenceConsumer.consume(new BatchSequence(changes));
+                    return completedFuture(task.state);
+                }
+                // different change id:s can come in different threads, but
+                // every individual change id row set should be delivered sequentially,
+                // because future chaining.
+                buffer.computeIfAbsent(change.getId(), (id) -> new ArrayList<>()).add(change);
+                return completedFuture(null);
+            }
+        });
+    }
+
     public static Consumer forTaskAndRawChangeConsumer(TaskAndRawChangeConsumer c) {
         return new Consumer(c);
     }
@@ -64,7 +93,22 @@ public class Consumer {
         });
     }
 
+    public static Consumer syncBatchSequenceConsumer(java.util.function.Consumer<BatchSequence> c) {
+        return new Consumer(new BatchSequenceConsumer() {
+            @Override
+            public CompletableFuture<Void> consume(BatchSequence event) {
+                c.accept(event);
+                return CompletableFuture.completedFuture(null);
+            }
+        });
+    }
+
+    public static Consumer forBatchSequenceConsumer(BatchSequenceConsumer c) {
+        return new Consumer(c);
+    }
+
     ConsumerDispatch getConsumerDispatch() {
         return consumerDispatch;
     }
+
 }
