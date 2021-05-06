29ddd6604fc1baefd6755aa2d9654787f71b119b
Piotr Grabowski <piotr.grabowski@scylladb.com>
2021-05-06 15:42:33 +0200

    Add MockRawChange class
    Add a new MockRawChange class which implements RawChange and allows us
    to construct an artificial RawChange without having to get one from
    the driver.
    
    This will allow us write tests that check different functionalities of
    the library without having to connect to Scylla.

---
+++ scylla-cdc-base/src/test/java/com/scylladb/cdc/model/worker/MockRawChange.java
@@ -0,0 +1,93 @@
+package com.scylladb.cdc.model.worker;
+
+import com.scylladb.cdc.model.worker.cql.Cell;
+import com.scylladb.cdc.model.worker.cql.Field;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class MockRawChange implements RawChange {
+    private final ChangeSchema changeSchema;
+    private final Map<String, Object> columnValues;
+
+    public MockRawChange(ChangeSchema changeSchema, Map<String, Object> columnValues) {
+        this.changeSchema = changeSchema;
+        this.columnValues = columnValues;
+    }
+
+    @Override
+    public ChangeSchema getSchema() {
+        return changeSchema;
+    }
+
+    @Override
+    public Object getAsObject(ChangeSchema.ColumnDefinition c) {
+        return columnValues.get(c.getColumnName());
+    }
+
+    @Override
+    public Cell getCell(ChangeSchema.ColumnDefinition c) {
+        return new Cell() {
+            @Override
+            public ChangeSchema.ColumnDefinition getColumnDefinition() {
+                return c;
+            }
+
+            @Override
+            public Set<Field> getDeletedElements() {
+                throw new UnsupportedOperationException("Not implemented yet");
+            }
+
+            @Override
+            public boolean hasDeletedElements() {
+                throw new UnsupportedOperationException("Not implemented yet");
+            }
+
+            @Override
+            public boolean isDeleted() {
+                throw new UnsupportedOperationException("Not implemented yet");
+            }
+
+            @Override
+            public ByteBuffer getUnsafeBytes() {
+                throw new UnsupportedOperationException("Not implemented yet");
+            }
+
+            @Override
+            public Object getAsObject() {
+                return MockRawChange.this.getAsObject(c);
+            }
+
+            @Override
+            public ChangeSchema.DataType getDataType() {
+                return c.getCdcLogDataType();
+            }
+        };
+    }
+
+    @Override
+    public boolean isNull(ChangeSchema.ColumnDefinition c) {
+        return getAsObject(c) == null;
+    }
+
+    @Override
+    public ByteBuffer getUnsafeBytes(ChangeSchema.ColumnDefinition c) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MockRawChange cells = (MockRawChange) o;
+        return changeSchema.equals(cells.changeSchema) &&
+                columnValues.equals(cells.columnValues);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(changeSchema, columnValues);
+    }
+}
