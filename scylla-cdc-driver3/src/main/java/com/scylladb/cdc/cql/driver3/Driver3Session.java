b0ac0a9bba184ca30033e6a29f67914c13113446
Piotr Grabowski <avelanarius@gmail.com>
2021-05-14 12:38:17 +0200

    Add configurable consistency level and local DC
    Add an option to configure the consistency level. The configured
    consistency level is used only for CDC log table queries. By default
    it is QUORUM.
    
    Add an option to configure the local DC name. By default it is not set,
    which does not change the prior behavior. If set, the started connection
    is with DCAwareRoundRobinPolicy with this local DC name.

--- scylla-cdc-driver3/src/main/java/com/scylladb/cdc/cql/driver3/Driver3Session.java
+++ scylla-cdc-driver3/src/main/java/com/scylladb/cdc/cql/driver3/Driver3Session.java
@@ -1,13 +1,16 @@
 package com.scylladb.cdc.cql.driver3;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
 import com.scylladb.cdc.cql.CQLConfiguration;
 
 public class Driver3Session implements AutoCloseable {
     private final Cluster driverCluster;
     private final Session driverSession;
+    private final ConsistencyLevel consistencyLevel;
 
     public Driver3Session(CQLConfiguration cqlConfiguration) {
         Cluster.Builder clusterBuilder = Cluster.builder()
@@ -20,12 +23,47 @@ public class Driver3Session implements AutoCloseable {
             clusterBuilder = clusterBuilder.withCredentials(user, password);
         }
 
+        if (cqlConfiguration.getLocalDCName() != null) {
+            clusterBuilder = clusterBuilder.withLoadBalancingPolicy(
+                    DCAwareRoundRobinPolicy.builder().withLocalDc(cqlConfiguration.getLocalDCName()).build());
+        }
+
         driverCluster = clusterBuilder.build();
         driverSession = driverCluster.connect();
+
+        switch (cqlConfiguration.getConsistencyLevel()) {
+            case ONE:
+                consistencyLevel = ConsistencyLevel.ONE;
+                break;
+            case TWO:
+                consistencyLevel = ConsistencyLevel.TWO;
+                break;
+            case THREE:
+                consistencyLevel = ConsistencyLevel.THREE;
+                break;
+            case QUORUM:
+                consistencyLevel = ConsistencyLevel.QUORUM;
+                break;
+            case ALL:
+                consistencyLevel = ConsistencyLevel.ALL;
+                break;
+            case LOCAL_QUORUM:
+                consistencyLevel = ConsistencyLevel.LOCAL_QUORUM;
+                break;
+            case LOCAL_ONE:
+                consistencyLevel = ConsistencyLevel.LOCAL_ONE;
+                break;
+            default:
+                throw new IllegalStateException("Unsupported consistency level: " + cqlConfiguration.getConsistencyLevel());
+        }
     }
 
     protected Session getDriverSession() {
         return driverSession;
+    }
+
+    protected ConsistencyLevel getConsistencyLevel() {
+        return consistencyLevel;
     }
 
     @Override
