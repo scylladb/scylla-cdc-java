023746eab8cbbd9053b0a232d7fb96aa5c64f051
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-03-05 01:28:16 -0400

    Make load balancing policy token aware when local-dc is provided

--- scylla-cdc-driver3/src/main/java/com/scylladb/cdc/cql/driver3/Driver3Session.java
+++ scylla-cdc-driver3/src/main/java/com/scylladb/cdc/cql/driver3/Driver3Session.java
@@ -2,6 +2,7 @@ package com.scylladb.cdc.cql.driver3;
 
 import com.datastax.driver.core.*;
 import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.policies.TokenAwarePolicy;
 import com.scylladb.cdc.cql.CQLConfiguration;
 import com.scylladb.cdc.cql.SslConfig;
 import io.netty.handler.ssl.SslContext;
@@ -113,7 +114,8 @@ public class Driver3Session implements AutoCloseable {
 
         if (cqlConfiguration.getLocalDCName() != null) {
             clusterBuilder = clusterBuilder.withLoadBalancingPolicy(
-                    DCAwareRoundRobinPolicy.builder().withLocalDc(cqlConfiguration.getLocalDCName()).build());
+                new TokenAwarePolicy(DCAwareRoundRobinPolicy.builder().withLocalDc(cqlConfiguration.getLocalDCName()).build())
+            );
         }
 
         if (cqlConfiguration.queryOptionsFetchSize > 0) {
