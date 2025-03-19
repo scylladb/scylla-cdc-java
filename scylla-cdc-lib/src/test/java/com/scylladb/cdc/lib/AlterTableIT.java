760a28899a2ddddaf42ee49d0de875c5c2a76ee9
Wojciech Bączkowski <wojciech.baczkowski@scylladb.com>
2025-03-19 23:39:26 +0100

    Add integration test altering table at runtime
    Checks for known issue with schema mismatch causing downstream
    issues when handling RawChanges.

---
+++ scylla-cdc-lib/src/test/java/com/scylladb/cdc/lib/AlterTableIT.java
@@ -0,0 +1,132 @@
+package com.scylladb.cdc.lib;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.InvalidTypeException;
+import com.google.common.base.Preconditions;
+import com.scylladb.cdc.model.TableName;
+import com.scylladb.cdc.model.worker.ChangeSchema;
+import com.scylladb.cdc.model.worker.RawChange;
+import com.scylladb.cdc.model.worker.RawChangeConsumer;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("integration")
+public class AlterTableIT {
+  Properties systemProperties = System.getProperties();
+  String hostname =
+      Preconditions.checkNotNull(systemProperties.getProperty("scylla.docker.hostname"));
+  int port = Integer.parseInt(systemProperties.getProperty("scylla.docker.port"));
+  String scyllaVersion =
+      Preconditions.checkNotNull(systemProperties.getProperty("scylla.docker.version"));
+
+  @Test
+  @Disabled // TODO: Enable when alters can be performed without downstream failures
+  public void alterBaseTableAtRuntime() {
+    String keyspace = "testks";
+    String table = "altertest";
+    Session session;
+    try (Cluster cluster = Cluster.builder().addContactPoint(hostname).withPort(port).build()) {
+      session = cluster.connect();
+      session.execute(
+          String.format(
+              "CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', "
+                  + "'replication_factor': 1};",
+              keyspace));
+      session.execute(String.format("DROP TABLE IF EXISTS %s.%s;", keyspace, table));
+      session.execute(
+          String.format(
+              "CREATE TABLE IF NOT EXISTS %s.%s (column1 int, column2 int, column3 int, PRIMARY"
+                  + " KEY (column1, column2)) WITH cdc = {'enabled': 'true'};",
+              keyspace, table));
+
+      AtomicInteger counter = new AtomicInteger(0);
+      // Start continuously populating the base table in the background
+      Runnable task =
+          () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+              PreparedStatement ps =
+                  session.prepare(
+                      String.format(
+                          "INSERT INTO %s.%s (column1, column2, column3) VALUES (?, ?, ?);",
+                          keyspace, table));
+              while (true) {
+                int current = counter.incrementAndGet();
+                session.execute(ps.bind(current, current, current));
+                try {
+                  // Makeshift rate limiting
+                  Thread.sleep(100);
+                } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+                }
+              }
+            }
+          };
+      Thread thread = new Thread(task);
+      thread.start();
+
+      AtomicBoolean failedDueToInvalidTypeEx = new AtomicBoolean(false);
+      RawChangeConsumer changeConsumer =
+          change -> {
+            try {
+              // printDetails(change)
+              String toString = change.toString();
+            } catch (Exception ex) {
+              failedDueToInvalidTypeEx.set(true);
+            }
+            return CompletableFuture.completedFuture(null);
+          };
+
+      try (CDCConsumer consumer =
+          CDCConsumer.builder()
+              .addContactPoint(new InetSocketAddress(hostname, port))
+              .addTable(new TableName(keyspace, table))
+              .withConsumer(changeConsumer)
+              .withQueryTimeWindowSizeMs(15 * 1000)
+              .withConfidenceWindowSizeMs(10 * 1000)
+              .withWorkersCount(1)
+              .build()) {
+        consumer.start();
+        Thread.sleep(35 * 1000);
+        session.execute(String.format("ALTER TABLE %s.%s " + "ADD column4 int;", keyspace, table));
+        Thread.sleep(20 * 1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      thread.interrupt();
+      assertFalse(failedDueToInvalidTypeEx.get());
+    }
+  }
+
+  private synchronized void printDetails(RawChange change) {
+    List<ChangeSchema.ColumnDefinition> list = change.getSchema().getAllColumnDefinitions();
+    System.out.println("RawChange details:");
+    for (ChangeSchema.ColumnDefinition cdef : list) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("column name: ")
+          .append(cdef.getColumnName())
+          .append(" column index: ")
+          .append(cdef.getIndex())
+          .append(" datatype: ")
+          .append(cdef.getCdcLogDataType());
+      if (cdef.isCdcColumn()) {
+        sb.append(" basedatatype: isCdcColumn");
+      } else {
+        sb.append(" basedatatype: ").append(cdef.getBaseTableDataType());
+      }
+      System.out.println(sb);
+    }
+    System.out.println(change.getAsObject("column1"));
+    System.out.flush();
+  }
+}
